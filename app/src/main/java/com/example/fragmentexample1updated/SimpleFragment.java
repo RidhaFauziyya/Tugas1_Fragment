b54feae6483257e8ea892d1e5542bf43e0ab2798
RidhaFauziyya <ridhafauziyyar123@gmail.com>
2023-02-23 22:26:26 +0700

    fragment dinamis

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -23,6 +23,10 @@ public class SimpleFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static SimpleFragment newInstance(){
+        return new SimpleFragment();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
